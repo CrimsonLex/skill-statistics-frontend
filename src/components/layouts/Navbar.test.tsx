b717b332fb4af8f9cd9895adfb332ecb0a6f31b0
CrimsonLex <alexadrito10@gmail.com>
2023-10-18 01:25:30 -0500

    Corrections added to testing

--- src/components/layouts/Navbar.test.tsx
+++ src/components/layouts/Navbar.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 
 describe('TenTopicsDTOTable', () => {
     it('it should render the component ', () => {
-        const { getByText } = render(<Navbar />);
+        const { getByText } = render(
+            <BrowserRouter>
+                <Navbar />
+            </BrowserRouter>
+        );
 
         const titleTable = getByText('Login');
 
